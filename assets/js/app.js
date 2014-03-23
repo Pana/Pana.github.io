cddc991936da7fa814e022209d03db88cafa35fb
pana <pana.wang@outlook.com>
2014-03-23 11:03:11 +0800

    new version

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,50 +1,21 @@
-/*
-* Coded by Pana
-* 2014-01-28
-*/
+$(function () {
+
+    $('.main').onepage_scroll({
+        sectionContainer: 'section',
+        easing: "ease",
+        animationTime: 1000,             
+        pagination: true,                
+        updateURL: false,                
+        beforeMove: function(index) {},  
+        afterMove: function(index) {
+            $('section .content').show();
+        },   
+        loop: false,                     
+        keyboard: true,                  
+        responsiveFallback: false 
+    });        
 
 
-$(function(){
-    var $win = $(window);
-    var winH = $win.height();
-    var winW = $win.width();
-
-
-    // 设置section高度
-    // $('section').height(winH);
-
-
-    var $v = $('video');
-    var v = $v[0];
-    v.volume = 0;
-
-
-    // h1居中, css是否可以实现 ?
-    var $h = $('h1');
-    var hH = $h.height();
-    var hW = $h.width();
-    $h.css('top', (winH - hH)/2 + 'px').css('left', (winW-hW)/2 + 'px');
-
-
-    console.log(hH, hW);
-
-
-    var $tmp = $('.chapter.bgblue');
-    var wheeling = false;
-    $(window).bind('mousewheel', function(event) {
-        if(wheeling) return;
-        wheeling = true;
-        if (event.originalEvent.wheelDelta >= 0) {
-            $tmp.removeClass('show');
-        } else {
-            $tmp.addClass('show')
-        }
-        setTimeout(function(){
-            wheeling = false;
-        }, 400);
-    });
-    
-
-
-
+    var video = $('video')[0];
+    video.volume = 0;
 });
